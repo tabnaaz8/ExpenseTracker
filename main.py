36865c3c2a529520814e8d790c026101de9f7696
Shaikh Khizar <khizarsajid@gmail.com>
2022-12-31 16:20:43 +0530

    added requirements and procfile

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ from expense_track import create_app
 
 if __name__ == '__main__':
     app = create_app()
-    app.run(debug=True)
+    app.run(debug=True, port=5000, host="0.0.0.0")
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
